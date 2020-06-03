281940567aded848e6a709e50fe9de978bb6011d
Nicolo <nicolorizzi1996@gmail.com>
2020-06-03 15:15:08 +0200

    UPDATE WITH BIND PARAMS

--- partials/update/server-update.php
+++ partials/update/server-update.php
@@ -12,20 +12,23 @@ $room_number = $_POST['room_number'];
 $beds = $_POST['beds'];
 $floor = $_POST['floor'];
 
-//UPDATE VALUE
 
+//UPDATE WITH BIND PARAM
 $sql = "UPDATE `stanze`
-SET `room_number` = $room_number,`beds` = $beds, `floor` = $floor
-WHERE `id` = $id_room";
+SET `room_number` = ?, `beds` = ?, `floor` = ?
+WHERE `id` = ?";
+$stmt = $connect->prepare($sql);
 
-//PERFORM QUERY
-$result = $connect->query($sql);
+$stmt->bind_param('iiii', $room_number, $beds, $floor, $id_room);
+
+$stmt->execute();
+
 
 //CHECK
 
-if ($result && $connect->affected_rows > 0) {
+if ($stmt && $connect->affected_rows > 0) {
     header("Location: $base_path/show.php?id=$id_room");
-} elseif ($result) {
+} elseif ($stmt) {
     die('Nessuna room trovata');
 } else {
     die('Si è verificato un errore');
