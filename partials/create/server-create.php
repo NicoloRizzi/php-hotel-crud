fe4a1eebf5846d7ba77e2af8d54dd999f8ecf8ba
Nicolo <nicolorizzi1996@gmail.com>
2020-06-03 17:10:58 +0200

    QUERY SQL CREATE

--- partials/create/server-create.php
+++ partials/create/server-create.php
@@ -6,3 +6,26 @@ include_once __DIR__ . '/../database.php';
 if ( empty($_POST['room_number'] ) || ( empty($_POST['beds'] ) ) || (empty($_POST['floor'])) ){
     die ('Missing parameters');
 }
+
+//ASSEGNAZIONE
+
+$room_number = $_POST['room_number'];
+$beds = $_POST['beds'];
+$floor = $_POST['floor'];
+
+//QUERY
+
+$sql = "INSERT INTO `stanze`
+(`room_number`, `beds`, `floor`, `created_at`, `updated_at`) VALUES (?,?,?, NOW(), NOW());";
+$stmt = $connect->prepare($sql);
+$stmt->bind_param('iii', $room_number, $beds, $floor);
+$stmt->execute();
+
+// CHECK DATA
+
+if($stmt && $stmt->insert_id) {
+    $id_room = $stmt->insert_id;
+    header("Location: $base_path" . "show.php?id=$id_room");
+} else {
+    die('Room creation error');
+}
